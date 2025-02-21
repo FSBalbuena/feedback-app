d0fefe026b767faa16698583cfe7b79b74068c05
FSBalbuena <fsbalbuena@gmail.com>
2025-02-21 19:07:07 -0300

    tests init

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+import { Config } from "jest";
+
+const config: Config = {
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts", "!**/vendor/**"],
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
+  transform: {
+    ".(ts|tsx)": ["ts-jest", { tsconfig: "tsconfig.app.json" }],
+  },
+  coverageReporters: ["json-summary", "clover", "text"],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/coverage",
+    "package.json",
+    "package-lock.json",
+    "pnpm-lock.json",
+    "jest.setup.ts",
+  ],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  },
+};
+
+module.exports = config;
